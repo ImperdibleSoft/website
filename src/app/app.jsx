ec144c65201a9ef6de4f489aa676a17442ba56b6
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2018-04-12 21:50:22 +0200

    7.1.0 Fixing Google Analytics

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import ReactGA from 'react-ga';
+import p from 'prop-types';
 
 import { ANALYTICS_TAG } from '../constants/branding';
 
@@ -12,32 +14,64 @@ import Messenger from './components/messenger';
 import Router from './router';
 
 const { NODE_ENV: ENV } = process.env;
+const debuggingGA = true;
+let initialized = false;
 
-// If not in development
-if (ENV !== 'dev') {
-  // Init Google Analytics
-  ReactGA.initialize(ANALYTICS_TAG);
-}
-
-// Location change handler
-const logPageView = () => {
-  // If not in development
-  if (ENV !== 'dev') {
-
-    // Update page stats
-    ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname);
+const log = (...args) => {
+  if (NODE_ENV === 'dev') {
+    console.log('[DEV]: ', ...args);
   }
 };
 
 class App extends React.Component {
-  render () {
+  componentDidMount() {
+    this.initReactGA();
+  }
+
+  componentDidUpdate(prevProps) {
+    const currPage = this.props.location.pathname;
+    const prevPage = prevProps.location.pathname;
+    log(prevProps.location, this.props.location);
+
+    if (prevPage !== currPage) {
+      this.logPageView();
+    }
+  }
+
+
+  initReactGA() {
+    // If not in development and not initialized
+    if (debuggingGA || (ENV !== 'dev' && !initialized)) {
+      initialized = true;
+
+      // Init Google Analytics
+      log(`Initializing Google Analytics tag ${ANALYTICS_TAG}`);
+      ReactGA.initialize(ANALYTICS_TAG, {
+        debug: debuggingGA,
+      });
+      this.logPageView();
+    }
+  }
+
+  // Location change handler
+  logPageView() {
+    // If not in development
+    if (debuggingGA || (ENV !== 'dev' && initialized)) {
+
+      // Update page stats
+      log(`Updating page view`);
+      ReactGA.set({ page: this.props.location.pathname });
+      ReactGA.pageview(this.props.location.pathname);
+    }
+  };
+
+  render() {
     return (
       <div className="App">
         <Header />
 
         <div className="App-content">
-          <Router onUpdate={logPageView} />
+          <Router onUpdate={this.logPageView} />
 
           <Support />
 
@@ -51,6 +85,12 @@ class App extends React.Component {
   }
 };
 
+App.propTypes = {
+  match: p.object.isRequired,
+  location: p.object.isRequired,
+  history: p.object.isRequired,
+};
+
 App.displayName = 'App';
 
-export default App;
+export default withRouter(App);
