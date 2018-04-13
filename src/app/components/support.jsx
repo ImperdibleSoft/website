8bfc34c7a61e2b27b4781e111ad816860e5629ac
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2018-04-13 08:38:24 +0200

    Logging clicks on Support section

--- src/app/components/support.jsx
+++ src/app/components/support.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import FA from '@fortawesome/react-fontawesome';
 import { faPatreon } from '@fortawesome/fontawesome-free-brands';
+import { logClick } from '../../utils/analytics';
 
 import { ABOUT } from '../../constants/app.routes';
 
 import Avatar from './avatar';
 import kumba from '../../images/kumba.jpg';
 import garona from '../../images/garona.jpg';
+
+const ctaTracking = {
+  category: 'Button Secondary',
+  action: 'User clicked in a secondary button',
+  label: 'Give them a treat!',
+};
+
+const linkTracking = {
+  category: 'Link',
+  action: 'User clicked in a link',
+  label: 'Meet them',
+};
 
 const Support = () => (
   <div className="App-section Support">
@@ -18,7 +31,7 @@ const Support = () => (
     <div className="App-side is-large">
       <p>Each time I get a new patron, Kumba and Garona get a surprise.</p>
 
-      <a className="Button Button--alt" href="https://www.patreon.com/imperdiblesoft/overview" target="_blank" rel="noopener noreferer">Give them a treat!</a> or <Link className="Link" to={`${ABOUT}#dogs`}>Meet them</Link>
+      <a className="Button Button--alt" href="https://www.patreon.com/imperdiblesoft/overview" target="_blank" rel="noopener noreferer" onClick={() => { logClick(ctaTracking); }}>Give them a treat!</a> or <Link className="Link" to={`${ABOUT}#dogs`} onClick={() => { logClick(linkTracking); }}>Meet them</Link>
     </div>
   </div>
 );
