066f3794a1a7c72749e9cd8dbec86fb337065ef0
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2018-04-14 12:07:06 +0200

    Improving loggers and prepearing for using sentry

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,13 +2,19 @@ const { NODE_ENV: ENV } = process.env;
 const shouldDebug = ENV === 'dev';
 
 export const log = (...args) => {
-  if (shouldDebug) console.log('[DEV]: ', ...args);
+  if (shouldDebug) console.log('[imperdiblesoft.com]: ', ...args);
+};
+
+export const info = (...args) => {
+  if (shouldDebug) console.info('[imperdiblesoft.com]: ', ...args);
 };
 
 export const warn = (...args) => {
-  if (shouldDebug) console.warn('[DEV]: ', ...args);
+  if (shouldDebug) console.warn('[imperdiblesoft.com]: ', ...args);
+  //else Log to Sentry
 };
 
 export const error = (...args) => {
-  if (shouldDebug) console.error('[DEV]: ', ...args);
+  if (shouldDebug) console.error('[imperdiblesoft.com]: ', ...args);
+  //else Log to Sentry
 };
